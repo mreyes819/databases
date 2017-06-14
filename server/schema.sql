5993747cc5c5bd8b110a65ee4684f8fb9649d4c7
mttrys <mttrys@gmail.com>
2017-06-13 19:29:54 -0700

    added SQl tables for users, rooms and messages

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,26 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(120) NOT NULL
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(120) NOT NULL
+);
 
-
-
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  userID INT,
+  roomID INT,
+  txt VARCHAR(120),
+  FOREIGN KEY(userID) REFERENCES users(id),
+  FOREIGN KEY(roomID) REFERENCES rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
